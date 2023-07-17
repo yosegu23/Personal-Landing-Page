4d30e626835624daf6a750ed3080c441381932d8
yosegu23 <36472632+yosegu23@users.noreply.github.com>
2023-07-18 01:26:54 +0700

    Add files via upload

---
+++ Yosegu/script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function(){
+    const navbar = document.querySelector('.navbar');
+    navbar.style.animation = 'none'; 
+    navbar.offsetHeight; 
+    navbar.style.animation = null;
+});
