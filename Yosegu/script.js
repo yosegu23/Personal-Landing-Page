adf5b37b29a04bdbaf2a16f63d1187438aa43ffe
yosegu23 <36472632+yosegu23@users.noreply.github.com>
2023-07-20 21:24:30 +0700

    Add files via upload
    Update Landing Page
    - Section Hero With Glitch
    - Responsive Tablets & Mobile
    - Responsive Social Media

--- Yosegu/script.js
+++ Yosegu/script.js
@@ -4,3 +4,16 @@ document.addEventListener('DOMContentLoaded', function(){
     navbar.offsetHeight; 
     navbar.style.animation = null;
 });
+
+// JavaScript to hide the splash screen after the website loads
+window.addEventListener("load", function () {
+    const splashScreen = document.querySelector(".splash-screen");
+    splashScreen.classList.add("hide");
+});
+
+const typing = new Typed(".dynamic-text", {
+  strings: ["Full Stack Developer", "UI/UX Designer", "Freelancer"],
+  typeSpeed: 100,
+  backSpeed: 40,
+  loop: true,
+  });
